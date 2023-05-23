2b2b5be5294ff860de70ac3c6c6a81e5e4914322
daniel salinas <133883829+thedracoxx@users.noreply.github.com>
2023-05-23 10:14:16 -0400

    Add files via upload

---
+++ daniel.py
@@ -0,0 +1,55 @@
+total=0
+opc1"a"
+while:opc1
+print(")
+print(La clínica dental El Diente de Oro)
+print(")
+print("1-calcular ")
+print("2-revelar")
+print("3-salir")
+while True:
+opc1-(input("seleccione una opcion"))
+if opc
+if(opc1 == "a" or opc1=="b" or opc1== "c")
+break
+else:
+print("ranura fuera de rango")
+else:
+print("error, dabe ingresar letra")
+if opc == "a":
+opc=0
+subtotal=0
+while opc1-1:
+print("calcular un tratamiento")
+print("1.-cerillas porcelana 250000")
+print("2.-inplantes dentales 475000")
+print("3.-ortodoncia brackets 800000")
+print("4.-salir")
+while true:
+try:
+opc = int(input("elija tratamiento"))
+if 1=opc
+break
+else:
+print("la opcion debe ser ")
+except ValueError:
+print("error debe ser numeros no caracteres")
+print("opcion fuera de rango")
+if opc == 1:
+subtotal=250000
+elif opc == 2:
+subtotal=475000
+elif opc == 3:
+subtotal=800000
+else:
+print("elija una opcion")
+while True:
+try:
+descuento int(input("1.-trabajo ausiliar \n2-trabajo admin \n3-trabajo docente"))
+print("opcion no esta")
+else:
+break
+except ValueError:
+print("Error")
+if descuento == 1:
+dato_
